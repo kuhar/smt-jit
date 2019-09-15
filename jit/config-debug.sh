7cc2c00ab64ec18cbead808039d657c567e57b70
Jakub Kuderski <kubakuderski@gmail.com>
2019-09-15 11:15:32 -0400

    Allow for build dir selection with config scripts

--- jit/config-debug.sh
+++ jit/config-debug.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
 set -e
+JIT_DIR=$(pwd)
 LLVM_DIR=$(readlink -f "$1")
 Z3_DIR=$(readlink -f "$2")
+BUILD_DIR="$3"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
 
-mkdir -p debug
-cd debug
-
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug \
+cmake "$JIT_DIR" -GNinja -DCMAKE_BUILD_TYPE=Debug \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
       -DCMAKE_C_COMPILER=clang-8 \
       -DCMAKE_CXX_COMPILER=clang++-8 \
